51267ef580e1d518956a46bd6cbbbbaf9ac29b00
Cauanz <61758981+Cauanz@users.noreply.github.com>
2024-09-05 17:10:00 -0300

    ✨ Reformula função e adiciona função para uso de client side cache

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,57 +1,93 @@
 const searchButton = document.querySelector('.searchButton');
 
+/* CACHE */
+function storeCache(url, data, headers){
+   const cacheEntry = {
+      data: data,
+      headers: {
+         // ETag: headers.get('ETag'),
+         // lastModified: headers.get('Last-Modified'),
+         cacheControl: headers.get('Cache-Control'),
+         // Expires: headers.get('Expires')
+      },
+      timeStamp: Date.now()
+   };
+   localStorage.setItem(url, JSON.stringify(cacheEntry));
+}
+
 
 /* RETORNO CONDIÇÃO ATUAL */
 async function getWeather(inputCity){
 
    const city = inputCity;
    const apiKey = 'ea9e8fdb5f074392af0160830232705';
-   // const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${encodeURIComponent(city)}&lang=pt`;
-
-   // fetch(apiUrl)
-   // .then((response) => response.json())
-   // .then((data) => {
-   //    console.log(data); // Teste de retorno de dados 
-
-      // const cityName = document.querySelector('.cityName');
-
-      // let city = data.location.name;
-      // let regiao = data.location.region;
-      // let pais = data.location.country;
-      // let temperature = data.current.temp_c;
-      // cityName.innerHTML = `${city} | ${regiao} | ${pais} | ${temperature}°C`;
-
-
-      // const condicao = document.querySelectorAll('.condicao');
-      // const condicaoIcones = document.querySelectorAll('.climaIcone');
-
-      // condicaoIcones.forEach((element) => {
-      //    const icon = data.current.condition.icon;
-      //    element.src = icon;
-      // });
-
-
-      // const condicaoAtual = data.current.condition.text;
-
-      // condicao.forEach((element) => {
-      //    element.innerHTML = `${condicaoAtual}`;
-      // });
-
-   // })
-   // .catch((error) => {
-   //    console.log(`Ocorreu um erro na solicitação dos dados ${error}`);
-   // });
-
-
    /* PREVISÃO DO TEMPO - HOJE, AMANHÃ E DEPOIS DE AMANHÃ */
-
-   /* FUNÇÃO AUTO INVOCADA - SÓ PARA FECHAR NO EDITOR */
    const forecastUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${encodeURIComponent(city)}&days=5&lang=pt`
 
-   fetch(forecastUrl)
-   .then((response) => response.json())
-   .then((data) => {
+   const cached = localStorage.getItem(forecastUrl);
+
+   if(cached){
+      const cacheEntry = JSON.parse(cached);
+      const cacheControl = cacheEntry.headers.cacheControl;
+      let maxAge = 0;
+   
+
+      if(cacheControl){
+         const matches = cacheControl.match(/max-age=(\d+)/);
+         
+         if (matches && matches[1]) {
+            maxAge = parseInt(matches[1], 10);
+         }
+      }
+      const age = (Date.now() - cacheEntry.timeStamp) / 1000;
+
+      if(age < maxAge){
+         return cacheEntry.data;
+      }
+   }
+
+   try {
+      
+      const response = await fetch(forecastUrl);
+      const headers = response.headers;
+      const data = await response.json();
+      console.log(headers.get('Cache-Control'));
+
+      return { headers, data, forecastUrl };
+   } catch (error) {
+      console.log(`Ocorreu um erro na função de aquisição dos dados ${error}`);
+   }
+
+}
+
+// function updateWeather(inputCity){
+//    const city = inputCity;
+//    const apiKey = 'ea9e8fdb5f074392af0160830232705';
+//    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${encodeURIComponent(city)}&lang=pt`
+
+//    fetch(apiUrl)
+//    .then((response) => response.json())
+//    .then((data) => {
+//       const temp = data.current.temp_c;
+//       const tempAtual = document.querySelectorAll('.temperaturaAtual');
+//       tempAtual.forEach((elemento) => {
+//          elemento.innerHTML = `${temp}°C`;
+//       })
+
+//    })
+//    .catch((error) => {
+//       console.log(`Ocorreu um erro na solicitação dos dados ${error}`);
+//    });
+// }
+
+
+const searchWeather = () => {
+   const inputCity = document.querySelector('.cityInput').value;
+   getWeather(inputCity)
+   .then(({ headers, data, forecastUrl }) => {
       // console.log(data); //* DEBUG
+
+      storeCache(forecastUrl, data, headers)
 
       const cityName = document.querySelector('.cityName');
 
@@ -126,34 +162,6 @@ async function getWeather(inputCity){
    }).catch((error) => {
       console.log(`Ocorreu um erro na solicitação dos dados ${error}`);
    });
-}
-
-// function updateWeather(inputCity){
-//    const city = inputCity;
-//    const apiKey = 'ea9e8fdb5f074392af0160830232705';
-//    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${encodeURIComponent(city)}&lang=pt`
-
-//    fetch(apiUrl)
-//    .then((response) => response.json())
-//    .then((data) => {
-//       const temp = data.current.temp_c;
-//       const tempAtual = document.querySelectorAll('.temperaturaAtual');
-//       tempAtual.forEach((elemento) => {
-//          elemento.innerHTML = `${temp}°C`;
-//       })
-
-//    })
-//    .catch((error) => {
-//       console.log(`Ocorreu um erro na solicitação dos dados ${error}`);
-//    });
-// }
-
-
-
-
-const searchWeather = () => {
-   const inputCity = document.querySelector('.cityInput').value;
-   getWeather(inputCity);
 }
 
 searchButton.addEventListener('click', (e) => {
